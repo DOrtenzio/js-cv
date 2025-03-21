5ed015184b6bccc6df072281e394c3ec7f67f854
Diego D'Ortenzio <diegodorte2007@gmail.com>
2025-03-21 09:48:48 +0100

    Correzione form

--- es/script.js
+++ es/script.js
@@ -10,18 +10,21 @@ function addNewTitolo(event) {
     h.innerHTML = "Titolo:";
     i = document.createElement("input");
     i.setAttribute("type", "text");
+    i.setAttribute("name", "titolo[]");
     i.setAttribute("placeholder","Laurea Magisterale");
     //Anno
     h1 = document.createElement("h5");
     h1.innerHTML = "Anno:";
     i1 = document.createElement("input");
     i1.setAttribute("type", "date");
+    i1.setAttribute("name", "anno[]");
     i1.setAttribute("placeholder","01-01-1500");
     //Luogo
     h2 = document.createElement("h5");
     h2.innerHTML = "Luogo:";
     i2 = document.createElement("input");
     i2.setAttribute("type", "text");
+    i2.setAttribute("name", "luogo[]");
     i2.setAttribute("placeholder","Curno, Monaco di Baviera, GE");
     //Bottone
     bott=document.createElement("button");
@@ -55,21 +58,21 @@ function addNewQualifica(event) {
     h.innerHTML = "Posto:";
     i = document.createElement("input");
     i.setAttribute("type", "text");
-    i.setAttribute("name", "posto");
+    i.setAttribute("name", "posto[]");
     i.setAttribute("placeholder","Tecnico");
     //Anno
     h1 = document.createElement("h5");
     h1.innerHTML = "Durata:";
     i1 = document.createElement("input");
     i1.setAttribute("type", "number");
-    i1.setAttribute("name", "durata");
+    i1.setAttribute("name", "durata[]");
     i1.setAttribute("placeholder","10");
     //Luogo
     h2 = document.createElement("h5");
     h2.innerHTML = "Luogo:";
     i2 = document.createElement("input");
     i2.setAttribute("type", "text");
-    i2.setAttribute("name", "luogo");
+    i2.setAttribute("name", "luogo[]");
     i2.setAttribute("placeholder","Braun GE");
     //Bottone
     bott=document.createElement("button");
